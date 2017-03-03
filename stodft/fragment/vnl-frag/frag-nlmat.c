f117bd3c333201b4a8bce4ee3780c59d18697ec5
mingchen <mingchenaabb@gmail.com>
2017-03-03 12:47:04 -0800

    continue

---
+++ stodft/fragment/vnl-frag/frag-nlmat.c
@@ -0,0 +1,37 @@
+/*==========================================================================*/
+/*cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc*/
+/*==========================================================================*/
+/*                                                                          */
+/*                         PI_MD:                                           */
+/*             The future of simulation technology                          */
+/*             ------------------------------------                         */
+/*                     Module: frag-nlmat.c                                 */
+/*                                                                          */
+/* This routine calculate the kinectic energy, non-local pseudo-potential   */
+/* energy and nuclei force correction.                                      */
+/*                                                                          */
+/*==========================================================================*/
+/*cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc*/
+/*==========================================================================*/
+#include "standard_include.h"
+#include "../typ_defs/typedefs_gen.h"
+#include "../typ_defs/typedefs_class.h"
+#include "../typ_defs/typedefs_bnd.h"
+#include "../typ_defs/typedefs_cp.h"
+#include "../typ_defs/typedefs_stat.h"
+#include "../proto_defs/proto_friend_lib_entry.h"
+#include "../proto_defs/proto_communicate_wrappers.h"
+#include "../proto_defs/proto_math.h"
+#include "../proto_defs/proto_frag_local.h"
+/*==========================================================================*/
+/*cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc*/
+/*==========================================================================*/
+calcNonLocalMatrix(CP *cp, CP *cpMini)
+/*========================================================================*/
+{/*begin routine*/
+/*========================================================================*/
+/*             Local variable declarations                                */
+
+
+
+
